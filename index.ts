3aa7bdcd2f207c0f2019a4d7abe5450df028e9b3
Ofonna-N <nwezeofonna@gmail.com>
2023-05-23 16:22:55 -0800

    installed nodemon and node-ts package for transpiling and watching ts files

---
+++ index.ts
@@ -0,0 +1,23 @@
+// class CustomLinkedList {
+//   #Node = class CustomNode {
+//     // value = 0;
+//     // next = null;
+//     constructor(val) {
+//       this.value = val;
+//       this.next = null;
+//     }
+//   };
+
+//   #first = null;
+//   #last = null;
+
+//   addLast() {
+//     const next = new this.#Node(10);
+//     console.log(next);
+//   }
+// }
+
+// const list = new CustomLinkedList();
+
+// list.addLast();
+console.log("heloo o");
